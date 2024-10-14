1232f7bfacc59a0f71090332b38242260fdd0a58
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-10-14 13:05:20 -0700

    little change

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const input = document.getElementById("text-value");
-const button = document.getElementByI("list-items");
+const button = document.getElementById("list-items");
 const listItems = document.getElementById("list-items");
 const inputError = document.getElementById("input_error");
+
+button.addEventListener("click", addTask);
+
+function addTask(){
+ const inputValue = input.value.trim();
+ input = "";
+ console.log(inputValue);
+}
