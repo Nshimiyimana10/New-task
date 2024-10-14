e76355340fac7f5cd77dadc36e62c482074aff38
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-10-14 12:53:06 -0700

    added new input error

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const input = document.getElementById("text-value");
 const button = document.getElementByI("list-items");
 const listItems = document.getElementById("list-items");
+const inputError = document.getElementById("input_error");
